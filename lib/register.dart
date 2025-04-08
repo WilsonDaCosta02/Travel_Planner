4c397c0a9b242f7b8be94dc109b9e2580ff8bb83
wilsondacosta0205 <dacostawillybrodus@gmail.com>
2025-04-08 21:25:21 +0800

    bikin tampilan awal dan penambahan background pada halaman register

--- lib/register.dart
+++ lib/register.dart
@@ -14,7 +14,7 @@ class _RegisterState extends State<Register> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 9, 119, 173),
+        backgroundColor: const Color.fromARGB(255, 34, 102, 141),
         title: Text(
           'Daftar Akun',
           style: TextStyle(
@@ -24,130 +24,142 @@ class _RegisterState extends State<Register> {
           ),
         ),
       ),
-      body: Center(
-        child: Dialog(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: SingleChildScrollView(
-            child: Container(
-              width: 500,
-              padding: EdgeInsets.all(20),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Container(
-                    width: 100,
-                    height: 100,
-                    decoration: BoxDecoration(
-                      color: const Color.fromARGB(255, 34, 102, 141),
-                      borderRadius: BorderRadius.circular(13),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Color.fromARGB(255, 34, 102, 141),
+        child: Center(
+          child: Dialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: SingleChildScrollView(
+              child: Container(
+                width: 500,
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Container(
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 34, 102, 141),
+                        borderRadius: BorderRadius.circular(13),
+                      ),
+                      child: Image.asset('assets/images/Frame 1.png'),
                     ),
-                    child: Image.asset('assets/images/Frame 1.png'),
-                  ),
 
-                  SizedBox(height: 40),
+                    SizedBox(height: 40),
 
-                  // === Nama Lengkap ===
-                  buildInputField(Icons.person, 'Nama :', 'Nama lengkap'),
+                    // === Nama Lengkap ===
+                    buildInputField(Icons.person, 'Nama :', 'Nama lengkap'),
 
-                  SizedBox(height: 10),
+                    SizedBox(height: 10),
 
-                  // === Nomor HP ===
-                  buildInputField(
-                    Icons.phone,
-                    'HP :',
-                    '08xxxxxxxxxx',
-                    keyboardType: TextInputType.phone,
-                  ),
-
-                  SizedBox(height: 10),
-
-                  // === Email ===
-                  buildInputField(Icons.email, 'Email :', 'example@gmail.com'),
-
-                  SizedBox(height: 10),
-
-                  // === Password ===
-                  Container(
-                    width: double.infinity,
-                    height: 35,
-                    decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 206, 206, 206),
-                      borderRadius: BorderRadius.circular(10),
+                    // === Nomor HP ===
+                    buildInputField(
+                      Icons.phone,
+                      'HP :',
+                      '08xxxxxxxxxx',
+                      keyboardType: TextInputType.phone,
                     ),
-                    child: Row(
-                      children: [
-                        SizedBox(width: 15),
-                        Icon(Icons.key),
-                        SizedBox(width: 15),
-                        Text('Password :'),
-                        Expanded(
-                          child: TextField(
-                            obscureText: _obscureText,
-                            style: TextStyle(color: Colors.black, fontSize: 16),
-                            decoration: InputDecoration(
-                              hintText: 'Masukkan Password',
-                              border: InputBorder.none,
-                              contentPadding: EdgeInsets.symmetric(
-                                horizontal: 8,
-                                vertical: 9,
+
+                    SizedBox(height: 10),
+
+                    // === Email ===
+                    buildInputField(
+                      Icons.email,
+                      'Email :',
+                      'example@gmail.com',
+                    ),
+
+                    SizedBox(height: 10),
+
+                    // === Password ===
+                    Container(
+                      width: double.infinity,
+                      height: 35,
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 206, 206, 206),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Row(
+                        children: [
+                          SizedBox(width: 15),
+                          Icon(Icons.key),
+                          SizedBox(width: 15),
+                          Text('Password :'),
+                          Expanded(
+                            child: TextField(
+                              obscureText: _obscureText,
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 16,
                               ),
-                              suffixIcon: IconButton(
-                                onPressed: () {
-                                  setState(() {
-                                    _obscureText = !_obscureText;
-                                  });
-                                },
-                                icon: Icon(
-                                  _obscureText
-                                      ? Icons.visibility
-                                      : Icons.visibility_off,
+                              decoration: InputDecoration(
+                                hintText: 'Masukkan Password',
+                                border: InputBorder.none,
+                                contentPadding: EdgeInsets.symmetric(
+                                  horizontal: 8,
+                                  vertical: 9,
+                                ),
+                                suffixIcon: IconButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      _obscureText = !_obscureText;
+                                    });
+                                  },
+                                  icon: Icon(
+                                    _obscureText
+                                        ? Icons.visibility
+                                        : Icons.visibility_off,
+                                  ),
                                 ),
                               ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    SizedBox(height: 20),
+
+                    // === Tombol Register ===
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromARGB(255, 99, 88, 220),
+                        minimumSize: Size(300, 35),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        'Register',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('Do you have account? '),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pop(context); // kembali ke login
+                          },
+                          child: Text(
+                            'Back to Login',
+                            style: TextStyle(
+                              color: const Color.fromARGB(255, 114, 33, 243),
                             ),
                           ),
                         ),
                       ],
                     ),
-                  ),
-
-                  SizedBox(height: 20),
-
-                  // === Tombol Register ===
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color.fromARGB(255, 99, 88, 220),
-                      minimumSize: Size(300, 35),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
-                    onPressed: () {},
-                    child: Text(
-                      'Register',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text('Do you have account? '),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.pop(context); // kembali ke login
-                        },
-                        child: Text(
-                          'Back to Login',
-                          style: TextStyle(
-                            color: const Color.fromARGB(255, 114, 33, 243),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
